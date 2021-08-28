86f07278b435ceec996f17a0435f87c0cbb8bdbc
Ajibola Salami <ayo.lolade@gmail.com>
2021-08-28 22:50:53 +0100

    update settings

--- blog/settings.py
+++ blog/settings.py
@@ -135,6 +135,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ayo.lolade@gmail.com'
-EMAIL_HOST_PASSWORD = 'ayobami1'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
